7b820d150268fa9778828dd460439c632f8528dc
SmuggingSpon <116337303+SmuggingSpon@users.noreply.github.com>
2024-10-06 11:23:03 +0700

    Delete Vocabulary Flashcard directory

--- Vocabulary Flashcard/Practice/Home/home.js
+++
@@ -1,6 +0,0 @@
-function sendNumberOfQuestions(){
-    document.cookie = "numberOfQuestions = " + document.getElementById("numberOfQuestions").value;
-    const xhhtp = new XMLHttpRequest();
-    xhhtp.open("POST", "home.php", true);
-    xhhtp.send();
-}
