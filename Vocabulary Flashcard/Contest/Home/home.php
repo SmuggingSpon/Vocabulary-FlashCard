998547390cb9e032d1bfb52e7753a15d03f27c05
SmuggingSpon <116337303+SmuggingSpon@users.noreply.github.com>
2024-12-31 18:34:24 +0700

    Delete Vocabulary Flashcard directory

--- Vocabulary Flashcard/Contest/Home/home.php
+++
@@ -1,5 +0,0 @@
-<?php
-    if(isset($_POST)){
-        $currentContestFile = fopen("../../Data/currentContest.txt", "w") or die("Unable to open file!");
-        fwrite($currentContestFile, $_COOKIE["sendData"]);
-    }
--- Vocabulary Flashcard/Create/create.js
+++
@@ -1,7 +0,0 @@
-function createDeck(){
-    document.cookie = "deckName = "+ document.getElementById("deck-name").value;
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "create.php", true);
-    xhttp.send();
-    location.reload();
-}
